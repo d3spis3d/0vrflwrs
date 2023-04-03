3255b5f49b45588377418c6b6fcb97d7e0edb67b
Daniel Budden <daniel.l.budden@gmail.com>
2023-04-03 12:25:06 +1000

    Fin Stage 3

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -5,69 +5,83 @@ use crate::persistance::{answers_dao::AnswersDao, questions_dao::QuestionsDao};
 
 mod handlers_inner;
 
+use handlers_inner::*;
+
+#[derive(Responder)]
+pub enum APIError {
+    #[response(status = 400)]
+    BadRequest(String),
+    #[response(status = 500)]
+    InternalError(String),
+}
+
+impl From<HandlerError> for APIError {
+    fn from(value: HandlerError) -> Self {
+        match value {
+            HandlerError::BadRequest(s) => Self::BadRequest(s),
+            HandlerError::InternalError(s) => Self::InternalError(s),
+        }
+    }
+}
+
 // ---- CRUD for Questions ----
 
 #[post("/question", data = "<question>")]
 pub async fn create_question(
     question: Json<Question>,
     questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
-) -> Json<QuestionDetail> {
-    Json(QuestionDetail {
-        question_uuid: "0001".to_owned(),
-        title: "Dummy Question".to_owned(),
-        description: "What's a quetion".to_owned(),
-        created_at: "Now".to_owned(),
-    })
+) -> Result<Json<QuestionDetail>, APIError> {
+    let question_detail =
+        handlers_inner::create_question(question.into_inner(), questions_dao).await?;
+
+    Ok(Json(question_detail))
 }
 
 #[get("/questions")]
 pub async fn read_questions(
     questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
-) -> Json<Vec<QuestionDetail>> {
-    Json(vec![QuestionDetail {
-        question_uuid: "0001".to_owned(),
-        title: "Dummy Question".to_owned(),
-        description: "What's a quetion".to_owned(),
-        created_at: "Now".to_owned(),
-    }])
+) -> Result<Json<Vec<QuestionDetail>>, APIError> {
+    let questions = handlers_inner::read_questions(questions_dao).await?;
+
+    Ok(Json(questions))
 }
 
 #[delete("/question", data = "<question_uuid>")]
 pub async fn delete_question(
     question_uuid: Json<QuestionId>,
     questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
-) {
+) -> Result<(), APIError> {
+    handlers_inner::delete_question(question_uuid.into_inner(), questions_dao).await?;
+
+    Ok(())
 }
 
 #[post("/answer", data = "<answer>")]
 pub async fn create_answer(
     answer: Json<Answer>,
     answers_dao: &State<Box<dyn AnswersDao + Send + Sync>>,
-) -> Json<AnswerDetail> {
-    Json(AnswerDetail {
-        answer_uuid: "0002".to_owned(),
-        question_uuid: "0001".to_owned(),
-        content: "An answer".to_owned(),
-        created_at: "Before".to_owned(),
-    })
+) -> Result<Json<AnswerDetail>, APIError> {
+    let answer_detail = handlers_inner::create_answer(answer.into_inner(), answers_dao).await?;
+
+    Ok(Json(answer_detail))
 }
 
 #[get("/answers", data = "<question_uuid>")]
 pub async fn read_answers(
     question_uuid: Json<QuestionId>,
     answers_dao: &State<Box<dyn AnswersDao + Send + Sync>>,
-) -> Json<Vec<AnswerDetail>> {
-    Json(vec![AnswerDetail {
-        answer_uuid: "0002".to_owned(),
-        question_uuid: "0001".to_owned(),
-        content: "An answer".to_owned(),
-        created_at: "Before".to_owned(),
-    }])
+) -> Result<Json<Vec<AnswerDetail>>, APIError> {
+    let answers = handlers_inner::read_answers(question_uuid.into_inner(), answers_dao).await?;
+
+    Ok(Json(answers))
 }
 
 #[delete("/answer", data = "<answer_uuid>")]
 pub async fn delete_answer(
     answer_uuid: Json<AnswerId>,
     answers_dao: &State<Box<dyn AnswersDao + Send + Sync>>,
-) {
+) -> Result<(), APIError> {
+    handlers_inner::delete_answer(answer_uuid.into_inner(), answers_dao).await?;
+
+    Ok(())
 }
