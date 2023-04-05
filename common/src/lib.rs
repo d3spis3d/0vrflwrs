7016765da794de8376ab62ee3cf5d3e24acd280a
Daniel Budden <daniel.l.budden@gmail.com>
2023-04-05 20:38:21 +1000

    Models to common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Question {
+    pub title: String,
+    pub description: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct QuestionDetail {
+    pub question_uuid: String,
+    pub title: String,
+    pub description: String,
+    pub created_at: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Serialize, Deserialize)]
+pub struct QuestionId {
+    pub question_uuid: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Answer {
+    pub question_uuid: String,
+    pub content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct AnswerDetail {
+    pub answer_uuid: String,
+    pub question_uuid: String,
+    pub content: String,
+    pub created_at: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AnswerId {
+    pub answer_uuid: String,
 }
