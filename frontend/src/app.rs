f657eca7497e22a02691e32ff5e550551a3c63e1
Daniel Budden <daniel.l.budden@gmail.com>
2023-04-05 23:04:28 +1000

    Add yew router

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,8 +1,25 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <h1>{ "Welcome to 0vrflwrs" }</h1>
+        },
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <h1>{ "Welcome to 0vrflwrs" }</h1>
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
